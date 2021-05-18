97bce2dc341c4d27b4be590818e45a7653af4ec0
danlei santos <danleisantos@hotmail.com>
2021-05-18 07:50:08 -0300

    alterando o tipo da data

--- migrations/20210514035940_create_table_users.js
+++ migrations/20210514035940_create_table_users.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('users', function(table){
         table.increments('id').primary()
         table.string('name').notNull()
-        table.datetime('date')
+        table.string('date')
         table.string('address').notNull()
         table.string('cpf')
         table.string('telephone').notNull()
